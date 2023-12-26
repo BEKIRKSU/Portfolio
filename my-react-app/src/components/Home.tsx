554665d98f8a618c7a47417faaaf6f23fbe9447e
BEKIRKSU <bekirkscu@gmail.com>
2023-12-26 17:25:11 +0000

    Update Home.tsx

--- my-react-app/src/components/Home.tsx
+++ my-react-app/src/components/Home.tsx
@@ -1,11 +1,11 @@
-// src/components/Home.tsx
+
 import React from 'react';
 
 const Home: React.FC = () => {
   return (
     <div>
-      <h2>Welcome to the Home Page!</h2>
-      <p>This is the home page of my portfolio website.</p>
+      <h2>Welcome to Bekir Kuscu's portfolio.</h2>
+      <p>I am currently a Graduate/Junior Software Engineer.</p>
     </div>
   );
 };
