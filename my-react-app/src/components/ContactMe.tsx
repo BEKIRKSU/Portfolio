02fb200d9d47cfea8fb9540f6bc6dfdd30bbb44d
BEKIRKSU <bekirkscu@gmail.com>
2023-12-30 18:04:11 +0000

    message box added

--- my-react-app/src/components/ContactMe.tsx
+++ my-react-app/src/components/ContactMe.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import LinkedIn from './LinkedIn';
 import './ContactMe.css';
@@ -8,8 +7,20 @@ const ContactMe: React.FC = () => {
     <div className="contact-container">
       <h2 className="contact-heading">Contact Me</h2>
       <p className="contact-paragraph">
-      Keep an eye out for the grand reveal! Meanwhile, connect with me on social media to be part of the conversation. 
+        Keep an eye out for the grand reveal! Meanwhile, connect with me on social media to be part of the conversation.
       </p>
+      <form action="#" method="post" className="contact-form">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" required />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" name="message" rows={4} required></textarea>
+
+        <button type="submit">Submit</button>
+      </form>
       <LinkedIn url="https://www.linkedin.com/" />
     </div>
   );
