894539e3270a09094de28a633ab074bd0a732d04
BEKIRKSU <bekirkscu@gmail.com>
2023-12-31 16:14:45 +0000

    -

---
+++ my-react-app/src/components/DownloadCV.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import LinkedIn from './LinkedIn';
+
+const DownloadCv: React.FC = () => {
+
+  const cvDownload = () => {
+    const resumePath = process.env.PUBLIC_URL + '../Assets/Bekir-Kuscu-Resume-CV.pdf'; 
+    window.open(resumePath, '_blank');
+
+  };
+
+  return (
+
+    <div>
+        <h1>Bekir's Resume/CV</h1>
+        <p>
+    Welcome to Bekir Kuscu resume/cv page! Below, you'll find details about my skills, experiences, and education.
+    Feel free to download my full resume/CV for a comprehensive overview.
+        </p>
+        <button onClick={cvDownload }>Download Resume/CV</button>
+        <LinkedIn url="https://www.linkedin.com/in/bekir-kuscu" />
+    </div>
+
+  );
+
+};
+
+ 
+
+export default DownloadCv;
--- my-react-app/src/components/Resume.tsx
+++ my-react-app/src/components/Resume.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Resume.css';
-import LinkedIn from './LinkedIn';
+import DownloadCv from './DownloadCV';
 
 const Resume: React.FC = () => {
   return (
@@ -9,9 +9,10 @@ const Resume: React.FC = () => {
       <p>To download Bekir's resume/cv please click on the link below or visit my LinkedIn profile. ADD MORE WORDS HERE AT LEAST 100 WORDSS ADD MORE WORDS HERE AT LEAST 100 WORDSS
       ADD MORE WORDS HERE AT LEAST 100 WORDSS ADD MORE WORDS HERE AT LEAST 100 WORDSS ADD MORE WORDS HERE AT LEAST 100 WORDSS ADD MORE WORDS HERE AT LEAST 100 WORDSS
       </p>
-      <LinkedIn url="https://www.linkedin.com/in/bekir-kuscu" />
     </div>
   );
 };
 
 export default Resume;
+
+
