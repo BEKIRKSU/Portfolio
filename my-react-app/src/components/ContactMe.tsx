eba18bd4f3dcbe7651825a7747f3fb029a7300e9
BEKIRKSU <bekirkscu@gmail.com>
2023-12-31 15:00:36 +0000

    Update ContactMe.tsx

--- my-react-app/src/components/ContactMe.tsx
+++ my-react-app/src/components/ContactMe.tsx
@@ -9,7 +9,8 @@ const ContactMe: React.FC = () => {
       <p className="contact-paragraph">
         Keep an eye out for the grand reveal! Meanwhile, connect with me on social media to be part of the conversation.
       </p>
-      <form action="#" method="post" className="contact-form">
+      <form action="https://api.web3forms.com/submit" method="post" className="contact-form">
+      <input type="hidden" name="access_key" value="e90e01aa-d270-440f-886c-c1b192d16d0e"/>
         <label htmlFor="name">Name:</label>
         <input type="text" id="name" name="name" required />
 
@@ -19,8 +20,10 @@ const ContactMe: React.FC = () => {
         <label htmlFor="message">Message:</label>
         <textarea id="message" name="message" rows={4} required></textarea>
 
+        <div className="h-captcha" data-captcha="true"></div>
         <button type="submit">Submit</button>
       </form>
+      <script src="https://web3forms.com/client/script.js" async defer></script>
       <LinkedIn url="https://www.linkedin.com/" />
     </div>
   );
