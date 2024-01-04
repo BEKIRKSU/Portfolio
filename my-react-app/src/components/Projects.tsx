bb5a0177af71ab38855357e12ae899be87dd4c9b
BEKIRKSU <bekirkscu@gmail.com>
2024-01-04 12:38:28 +0000

    Projects added

--- my-react-app/src/components/Projects.tsx
+++ my-react-app/src/components/Projects.tsx
@@ -3,27 +3,72 @@ import './Projects.css';
 import LanguagesIcon from './LanguagesIcon';
 
 const Projects: React.FC = () => {
-  const handleLinkClick = () => {
-    window.open('https://www.mmcode.io/', '_blank');
+  const handleLinkClick = (link: string) => {
+    window.open(link, '_blank');
   };
 
   return (
     <div className="work-container">
-      <h1 className="work-heading">Bekir Kuscu's Projects</h1>
+      <h1 className="work-heading">BEKIR KUSCU / Projects</h1>
       <ol className="cool-list">
-        <li 
-          className="cool-list-item"
-          onClick={handleLinkClick}
-        >
-          MM-CODE / 2023 - 2024
-          </li>
-          <p>Powered by a diverse team of advisors and contributors, 
-            we tackled coding challenges with focus and repetition in an Agile environment. </p>
-            <LanguagesIcon> React.js (JavaScript) </LanguagesIcon>  <LanguagesIcon> SQL </LanguagesIcon> <LanguagesIcon> CSS5 </LanguagesIcon> 
-             <LanguagesIcon> Node.js </LanguagesIcon> <LanguagesIcon> Bootstrap </LanguagesIcon> <LanguagesIcon> Auth0 </LanguagesIcon>
+        <li className="cool-list-item" onClick={() => handleLinkClick('https://www.mmcode.io/')}>
+          MM-Code / 2023 - 2024
+        </li>
+        <p>
+          Powered by a diverse team of advisors and contributors, we tackled coding challenges with focus and repetition in an Agile environment.
+        </p>
+        <LanguagesIcon>React.js (JavaScript)</LanguagesIcon>
+        <LanguagesIcon>SQL</LanguagesIcon>
+        <LanguagesIcon>HTML5</LanguagesIcon>
+        <LanguagesIcon>CSS3</LanguagesIcon>
+        <LanguagesIcon>Node.js</LanguagesIcon>
+        <LanguagesIcon>Bootstrap</LanguagesIcon>
+        <LanguagesIcon>Auth0</LanguagesIcon>
+
+        <li className="cool-list-item" onClick={() => handleLinkClick('https://animalfarm-qqai.onrender.com/')}>
+          Booking & Management System / 2023
+        </li>
+        <p>
+          -
+        </p>
+        <LanguagesIcon>React.js (JavaScript)</LanguagesIcon>
+        <LanguagesIcon>SQL</LanguagesIcon>
+        <LanguagesIcon>HTML5</LanguagesIcon>
+        <LanguagesIcon>CSS3</LanguagesIcon>
+
+      <li className="cool-list-item" onClick={() => handleLinkClick('https://github.com/Mr-DEM1R/1.618_AI')}>
+          Eco-Scan / 2023
+        </li>
+        <p>
+          -
+        </p>
+        <LanguagesIcon>Python</LanguagesIcon>
+        <LanguagesIcon>OpenCV</LanguagesIcon>
+        <LanguagesIcon>YoloV8</LanguagesIcon>
+        <LanguagesIcon>Kaggle</LanguagesIcon>
+        <LanguagesIcon>Roboflow</LanguagesIcon>
+        <LanguagesIcon>Streamlit</LanguagesIcon>
+
+        <li className="cool-list-item" onClick={() => handleLinkClick('https://github.com/BEKIRKSU/CV.O')}>
+          CVO / 2023
+        </li>
+        <p>
+          -
+        </p>
+        <LanguagesIcon>React.js</LanguagesIcon>
+        <LanguagesIcon>HTML5</LanguagesIcon>
+        <LanguagesIcon>CSS3</LanguagesIcon>
+
+      <li className="cool-list-item" onClick={() => handleLinkClick('https://github.com/BEKIRKSU/snake_game')}>
+          Classic Snake Game / 2023
+        </li>
+        <p>
+          -
+        </p>
+        <LanguagesIcon>Python</LanguagesIcon>
       </ol>
-{/* 
-      <h2>Coming Soon:</h2>
+
+      {/* <h2>Coming Soon:</h2>
       <p className="work-paragraph">
         Bekir Kuscu's Portfolio Website. This space showcases my projects, skills, and journey to becoming a Software Engineer. If you're a fellow programmer, tech enthusiast, or someone just curious about how things work, this space is for you.
       </p>
@@ -36,7 +81,7 @@ const Projects: React.FC = () => {
       <h2>Tech Stack Showcase:</h2>
       <p className="work-paragraph">
         Discover the tools I use to create my work, and the technologies I love to work with.
-      </p> */}
+      </p>  */}
 
       <h3>Explore my GitHub below for a closer look at my projects and contributions.</h3>
       <a
