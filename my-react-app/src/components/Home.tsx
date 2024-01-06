8918b3d18921cfcec8602c99f362a57d3393c2bb
BEKIRKSU <bekirkscu@gmail.com>
2024-01-06 15:48:08 +0000

    -

--- my-react-app/src/components/Home.tsx
+++ my-react-app/src/components/Home.tsx
@@ -11,6 +11,7 @@ const Home: React.FC = () => {
   <div className="line"></div>
   <div className="line"></div>
 </div>
+<div>
       <h1 className="home-heading">Welcome to Bekir Kuscu's portfolio.</h1>
       {/* <p className="home-paragraph"> Hello, I'm Bekir Kuscu, a passionate Graduate/Junior Software Engineer with over a year of hands-on experience in technologies such as React JS, C#, JavaScript, TypeScript, and SQL. Throughout my journey, I've honed my skills and cultivated a deep understanding of these tools, enabling me to take on complex challenges and contribute meaningfully to software development projects.</p>
       <p className="home-paragraph">Now, with a sense of excitement and anticipation, I'm thrilled to announce that something truly exciting is in the works! In the coming days, I'll be giving a glimpse into my world of coding, creativity, and innovation. This endeavor serves as a testament to my commitment to pushing the boundaries in software engineering.</p>
@@ -21,6 +22,7 @@ const Home: React.FC = () => {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png"
           alt="GitHub"
         />
+        </div>
     </div>
   );
 };
